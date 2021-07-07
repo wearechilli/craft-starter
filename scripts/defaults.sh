9df131b679ba395c62fde667582819addf4ea40a
Sten Van den Bergh <dev@stenvdb.be>
2021-07-07 17:54:16 +0200

    Create defaults.sh

---
+++ scripts/defaults.sh
@@ -0,0 +1,9 @@
+# Stop when no branch was specified
+if [[ -z "$1" ]]; then
+  echo "Please specify the branch to pull from eg. 'npm run <task> <main>'"
+  exit 1
+fi
+# Get env variables (See: https://stackoverflow.com/a/30969768/440201)
+set -o allexport
+source "./.env.db.$1"
+set +o allexport
