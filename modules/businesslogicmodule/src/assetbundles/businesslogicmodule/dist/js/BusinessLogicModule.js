25d9c9e2f3ebf51dad392022c0b8f65bd26999b9
Sten Van den Bergh <dev@stenvdb.be>
2021-07-19 11:43:45 +0200

    Remove nuxt gitignore lines

---
+++ modules/businesslogicmodule/src/assetbundles/businesslogicmodule/dist/js/BusinessLogicModule.js
@@ -0,0 +1,11 @@
+/**
+ * Business Logic module for Craft CMS
+ *
+ * Business Logic JS
+ *
+ * @author    Sten Van den Bergh
+ * @copyright Copyright (c) 2020 Sten Van den Bergh
+ * @link      https://stenvdb.be
+ * @package   BusinessLogicModule
+ * @since     1.0.0
+ */
