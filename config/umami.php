124b80850595d0d1528d004228e6fa2d9ce4300e
Sten Van den Bergh <dev@stenvdb.be>
2021-08-11 16:56:37 +0200

    Umami 🍄

--- config/umami.php
+++ config/umami.php
@@ -3,19 +3,19 @@
 return array(
     '*' => array(
         // The domain name where Fathom is hosted. This is also the URL where the tracker code is pointed to.
-        'baseUri' => '$FATHOM_BASE_URI',
+        'baseUri' => '$UMAMI_BASE_URI',
 
         // The tracking ID of this site. You can find the ID in your tracking code snippet, e.g.: ABCDE
-        'trackingId' => '$FATHOM_TRACKING_ID',
+        'websiteId' => '$UMAMI_WEBSITE_ID',
 
-        'username' => '$FATHOM_USERNAME',
+        'username' => '$UMAMI_USERNAME',
 
-        'password' => '$FATHOM_PASSWORD',
+        'password' => '$UMAMI_PASSWORD',
 
         // Automatically inject a tracking script in your site
         'injectTracking' => false,
     ),
     'production' => [
         'injectTracking' => true,
-	],
+    ],
 );
